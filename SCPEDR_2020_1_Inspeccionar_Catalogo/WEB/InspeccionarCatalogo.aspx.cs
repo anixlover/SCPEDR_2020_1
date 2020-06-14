2c16d9b3158ae0db977a260578d9aaa73633ddc1
anixlover <jhosselyn7@gmail.com>
2020-06-14 14:20:03 -0500

    V2.1

---
+++ SCPEDR_2020_1_Inspeccionar_Catalogo/WEB/InspeccionarCatalogo.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CTR;
+using DTO;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Catalogo :  System.Web.UI.Page
+{
+    CtrTipoMoldura objCtrTipoMoldura = new CtrTipoMoldura();
+    DtoTipoMoldura objDtoTipoMoldura = new DtoTipoMoldura();
+    DtoMoldura objDtoMoldura = new DtoMoldura();
+    Log _log = new Log();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            try
+            {
+                
+
+            }
+            catch (Exception ex)
+            {
+                _log.CustomWriteOnLog("InspeccionarCatalogo", "Error = " + ex.Message + "Stac" + ex.StackTrace);
+
+                throw;
+            }
+
+
+        }
+    }
+}
